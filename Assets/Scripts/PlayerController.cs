b5b651de6301ce316a8b6e07b5be6c8a1e3ddc61
Gajam_Jyothsna <jyothsna.gajam@yesgnome.com>
2022-05-01 00:23:07 +0530

    Player Animations and Movement is added.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController characterController;
+    public float playerSpeed;
+    Animator animator;
+    public float playerRotateSpeed;
+    public int health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal") * playerSpeed;
+        float inputZ = Input.GetAxis("Vertical") * playerSpeed;
+
+        Vector3 movement = new Vector3(inputX, 0f, inputZ);
+
+        characterController.Move(movement * Time.deltaTime);
+
+        animator.SetFloat("Speed", movement.magnitude);
+        /*
+        if(movement.magnitude > 0f)
+
+        {
+            Quaternion tempDirection = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, tempDirection, Time.deltaTime * playerRotateSpeed);
+
+        }*/
+
+        transform.Rotate(Vector3.up, inputX * playerRotateSpeed * Time.deltaTime);
+        if (inputZ != 0)
+        {
+            characterController.SimpleMove(transform.forward * Time.deltaTime * inputZ);
+
+        }
+
+
+
+    }
+}
